94d7ff4df1c85187c7f7fe825e8b16e67ab1672e
Colin797 <colin797@tamu.edu>
2020-11-14 18:24:50 -0600

    Final VSM

--- quickeats/src/vsm.js
+++ quickeats/src/vsm.js
@@ -4,6 +4,22 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import React from "react";
 
+function match(a,b){
+  if(b.includes(" ")){
+    return a.includes(b)
+  }
+  else{
+    var wordsInIngredients = a.split(" ");
+    for(let i = 0; i < wordsInIngredients.length; i++){
+     if(wordsInIngredients[i] == b){
+      return true;
+     }
+    }
+    return false;
+  }
+  
+}
+
 function Vsm(props) {
   
   var json = require('./data/recipes_raw_nosource_epi.json')
@@ -11,14 +27,13 @@ function Vsm(props) {
 
   //var userQuery = props.query;
   console.log("vsm")
-  var userQuery = ["salmon","tomato","lemon","garlic","butter"]
+  var userQuery = ["chicken"]
   //var userQuery = ["onions","pepper","butter"]
   //var userQuery = ["chicken","mustard","pepper","onions","potato"]
   var scoreMap = new Map()
   var timeMap = new Map()
   var instructions
   var ingredients
-
 
   for (const [key, value] of Object.entries(json)) {
     let score = 0
@@ -27,13 +42,16 @@ function Vsm(props) {
     try {
           for(let i = 0; i < userQuery.length; i++){
             for(let j = 0; j < value.ingredients.length; j++){
-              if(value.ingredients[j].includes(userQuery[i])){ //modifty to only look for userQuery
+              if(match(value.ingredients[j], userQuery[i])){ 
                 score += 1
                 break 
               }
             }
           }
-
+          if(score == 0){
+            continue
+          }
+         
           instructions = value.instructions
 
           var temp = instructions.replaceAll("."," ")
@@ -75,25 +93,26 @@ function Vsm(props) {
               scoreMap.set(key,score)
               timeMap.set(key,time)
             }
-    } catch (error) {
-    }
+    } catch (error) {}    
   }
 
-
-/*for (const [key, value] of Object.entries(json1)) {
+  /*for (const [key, value] of Object.entries(json1)) {
     let score = 0
       //for ar
       //length_in.push(value.ingredients.length-1)
     try {
           for(let i = 0; i < userQuery.length; i++){
             for(let j = 0; j < value.ingredients.length; j++){
-              if(value.ingredients[j].includes(userQuery[i])){ //modifty to only look for userQuery
+              if(match(value.ingredients[j], userQuery[i])){ 
                 score += 1
                 break 
               }
             }
           }
-
+          if(score == 0){
+            continue
+          }
+         
           instructions = value.instructions
 
           var temp = instructions.replaceAll("."," ")
@@ -128,16 +147,16 @@ function Vsm(props) {
               }
             }
 
-            score -= ( value.ingredients.length - 1 - score ) * .5
+            score -= ( value.ingredients.length - score ) * .5
 
             if ( time != 0 && !isNaN(time) ) {
               score += (eval(1/time))
               scoreMap.set(key,score)
               timeMap.set(key,time)
             }
-    } catch (error) {
-    }
+    } catch (error) {}    
   }*/
+
 
   var keyList = []
   var scoreList = []
@@ -147,7 +166,9 @@ function Vsm(props) {
   var instructionsList= []
 
   var x
-  for (let i = 0; i < 10; i++) {
+  var numResults = scoreMap.size
+
+  for (let i = 0; i < numResults; i++) {
     x = ([...scoreMap.entries()].reduce((a, e ) => e[1] > a[1] ? e : a))
     var time = timeMap.get(x[0])
 
@@ -180,8 +201,49 @@ function Vsm(props) {
     scoreMap.delete(x[0])
   }
 
-  return (
-
+  if(numResults == 0){
+    return (
+      <Typography style={{color:"black", margin:"50px"}}>
+      <Card style={{backgroundColor:"#3f51b5", color:"#fff", marginBottom:"20px"}}> 
+        <div style={{fontSize:"20px", paddingLeft:"10px", paddingRight:"10px"}}> Sorry, we did not find any recipes that include any of your ingredients. Please try again with different ingredients. </div>
+      </Card>
+      
+      </Typography>
+      );
+  }
+  else if(numResults == 1){
+    return (
+      <Typography style={{color:"black", margin:"50px"}}>
+        <Card style={{backgroundColor:"#3f51b5", color:"#fff", marginBottom:"20px"}}> 
+          <h1 style={{fontSize:"25px"}}> Recipe Title: {titleList[0]} </h1>
+          <div style={{paddingBottom:"10px"}}> <h2>Ingredients:</h2> { ingredientsList[0] }  </div>
+          <div style={{paddingBottom:"10px"}}> <h2>Time to Prepare:</h2> {timeList[0]} minutes</div>
+          <div style={{paddingBottom:"10px"}}> <h2>Instructions:</h2> {instructionsList[0]}</div>
+        </Card>
+      </Typography>
+      );
+  }
+  else if(numResults == 2){
+    return (
+      <Typography style={{color:"black", margin:"50px"}}>
+        <Card style={{backgroundColor:"#3f51b5", color:"#fff", marginBottom:"20px"}}> 
+          <h1 style={{fontSize:"25px"}}> Recipe Title: {titleList[0]} </h1>
+          <div style={{paddingBottom:"10px"}}> <h2>Ingredients:</h2> { ingredientsList[0] }  </div>
+          <div style={{paddingBottom:"10px"}}> <h2>Time to Prepare:</h2> {timeList[0]} minutes</div>
+          <div style={{paddingBottom:"10px"}}> <h2>Instructions:</h2> {instructionsList[0]}</div>
+        </Card>
+      
+        <Card style={{backgroundColor:"#3f51b5", color:"#fff", marginBottom:"20px"}}> 
+          <h1 style={{fontSize:"25px"}}> Recipe Title: {titleList[1]} </h1>
+          <div style={{paddingBottom:"10px"}}> <h2>Ingredients:</h2> {ingredientsList[1]} </div>
+          <div style={{paddingBottom:"10px"}}> <h2>Time to Prepare:</h2> {timeList[1]} minutes</div>
+          <div style={{paddingBottom:"10px"}}> <h2>Instructions:</h2> {instructionsList[1]}</div>
+        </Card>
+      </Typography>
+      );
+  }
+  else{
+    return (
     <Typography style={{color:"black", margin:"50px"}}>
       <Card style={{backgroundColor:"#3f51b5", color:"#fff", marginBottom:"20px"}}> 
         <h1 style={{fontSize:"25px"}}> Recipe Title: {titleList[0]} </h1>
@@ -204,8 +266,10 @@ function Vsm(props) {
         <div style={{paddingBottom:"10px"}}> <h2>Instructions:</h2> {instructionsList[2]}</div>
       </Card>
       </Typography>
-    //<div> {props.query} </div>
+    
   );
+ }
+  
 }
 
 export default Vsm;
